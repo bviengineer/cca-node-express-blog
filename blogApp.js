b1c2cb851bcff54add1aa015935a2f185b5d5b46
BVIDev <16726593+bviengineer@users.noreply.github.com>
2019-02-10 23:22:07 -0500

    Update console.log and remove comments no longer needed

--- blogApp.js
+++ blogApp.js
@@ -12,20 +12,7 @@ const express = require("express"),
 
 //var port = process.env.port || 3000; 
 
-//const cool = require('cool-ascii-faces')
-//const express = require('express')
-//const path = require('path')
 const port = process.env.PORT || 5000 
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .get('/cool', (req, res) => res.send(cool()))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
 
 //Blog repository related 
 var router = express.Router();
@@ -169,5 +156,5 @@ app.get("/html", (req, res) => {
 //Routes END here
 
 app.listen(port); 
-console.log("The server is runnig on port 3000");
+console.log("The server is runnig on port ", port);
 readSubmissionMessages();
