50500b7a5df77c2aab1fbec6baa1cd038ead9ec5
BVIDev <16726593+bviengineer@users.noreply.github.com>
2019-02-10 23:07:10 -0500

    Addexpress module and settings

--- blogApp.js
+++ blogApp.js
@@ -12,10 +12,20 @@ const express = require("express"),
 
 //var port = process.env.port || 3000; 
 
-//const cool = require('cool-ascii-faces')
+const cool = require('cool-ascii-faces')
 //const express = require('express')
 //const path = require('path')
 const port = process.env.PORT || 5000 
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 
 //Blog repository related 
 var router = express.Router();
